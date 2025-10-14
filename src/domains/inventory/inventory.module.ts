48cf85083821adcbee6886b3af5aa2faf1e07478
Ayomide Ojo <ayomidedaniel00@gmail.com>
2025-10-14 19:50:01 +0100

    fix: e2e tests

--- src/domains/inventory/inventory.module.ts
+++ src/domains/inventory/inventory.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductsModule } from '../products/products.module';
+import { WarehousesModule } from '../warehouses/warehouses.module';
 import { ProductSupplier } from './entities/product-supplier.entity';
 import { WarehouseStock } from './entities/warehouse-stock.entity';
 import { InventoryController } from './inventory.controller';
@@ -11,6 +12,7 @@ import { InventoryListener } from './listeners/inventory.listener';
 @Module({
   imports: [
     TypeOrmModule.forFeature([ProductSupplier, WarehouseStock]),
+    WarehousesModule,
     ProductsModule,
   ],
   providers: [InventoryService, InventoryListener],
